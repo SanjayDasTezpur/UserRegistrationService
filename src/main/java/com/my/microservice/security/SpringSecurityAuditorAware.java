f569f41552595a4d047fa16207f3f1fe7e492a8f
SanjayDasTezpur <sanjaydas.32100@gmail.com>
2018-12-07 23:34:25 +0530

    Initial application generated by JHipster-5.7.0

---
+++ src/main/java/com/my/microservice/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package com.my.microservice.security;
+
+import com.my.microservice.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
